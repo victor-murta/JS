ac8bab5bf6bcb23f6ded5130a2ef8b4ed5189159
victor-murta <vmurtag16@gmail.com>
2021-05-15 16:57:05 -0300

    Desenvolvimento avançado

--- DesenvolvimentoAvancado/main.js
+++ DesenvolvimentoAvancado/main.js
@@ -1,4 +1,4 @@
-
+/*
 //funções anônimas
 var soma = function(a, b) {
     return a + b;
@@ -26,8 +26,86 @@ var obj = {
 }
 
 obj.showContent();
+*/
+
+// default funciton args
+
+function randomNumber(){
+    return Math.round(Math.random() * 10);
+}
+
+function multiply(a, b = /*1*/ /*a*/ randomNumber()){
+    //caso b não seja passado
+    //b = b || 1;
+    // b = typeof b === undefined ? 1 : b;
+    // if ( typeof b === undefined){
+    //     b = 1;
+    // }
+    console.log(a * b);
+}
+
+// multiply(5);  // 0 é convertido para o false
 
 
-// 19, 68, 116, 103, 96, 84, 92
-// 14, 47, 87 , 76 , 69, 57, 63
+var obj = {
+    // sum : function sum(a, b){
+    //     console.log( a + b);
+    // }
+
+    sum(a, b){
+        console.log(a + b);
+    }
+
+};
+
+// obj.sum(1, 5);
+
+// rest e spread operators
+
+// function sum(...args){
+//     // return args.reduce((acc, value) => acc + value, 0);  0 == valor inicial; acc == valor acumulado; value == valor 
+
+// }
+
+
+// //rest operator 
+// const sum = (a,b,...rest) => {
+//     return rest;
+// }
+
+
+// //spread operator
+// const str = 'Digital';
+// function log(...args){
+//     console.log(args);
+// }
+
+// console.log(sum(2,4,5,6,7));
+
+
+// log(...str)
+
+
+//Destructuring em reactjs
+
+// var arr = ['Apple', 'Banana', 'Orange'];
+
+// // var apple = arr[0];
+
+
+// //criando variáveis para suas respectivas posições
+// var [apple, banana, orange, [array]] = ['Apple', 'Banana', 'Orange', ['tomato']];
+
+// // console.log(banana.indexOf(banana))
+// console.log(array)
+
+
+// Introdução a generators
+
+
+const uniqueId = Symbol('Testando'); //identificador 
+
+
+console.log(uniqueId);
+
 
